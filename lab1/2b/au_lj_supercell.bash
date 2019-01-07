0fa19d212fc40add05900af0e4003f437361aecf
Austin Zadoks <zooks97@gmail.com>
2019-01-06 20:33:40 -0500

    Lab 1 question 2 progress

--- lab1/2b/au_lj_supercell.bash
+++ lab1/2b/au_lj_supercell.bash
@@ -22,31 +22,32 @@ do
     rm $runname"_supercell.temp"
 done
 
-# write input files for supercell with vacancy
+# write input files for supercell with vacancy relax
 echo "VACANCY CALCULATIONS"
 for cell in "${cellsize[@]}";
 do
     echo $cell  # uncomment to print cell size at each step
     echo -e \
 "4.077 Au "$cell" true\n"\
-"single dist comp conp\n"\
+"opti dist comp conp\n"\
 "lennard 12 6\n"\
 "Au core Au core 214108.2 625.482 40.0 0 0\n"\
-        > $runname"_vacancy.temp"
+        > $runname"_relax.temp"
     # run buildcell
-    ./buildcell.py $runname"_vacancy.temp"
-    rm $runname"_vacancy.temp"
+    ./buildcell.py $runname"_relax.temp"
+    rm $runname"_relax.temp"
 done
 
+# run gulp
 for file in *.in; do
     gulp < $file > $(basename $file .in).out
 done
 
 # parse output files
 grep "Total lattice energy" -m 1 *supercell*out > supercell.dat
-grep "Total lattice energy" -m 1 *vacancy*.out > vacancy.dat
-paste vacancy.dat supercell.dat > $runname.dat
-rm vacancy.dat supercell.dat
+grep "Final energy" -m 1 *relax*.out > relax.dat
+paste supercell.dat relax.dat > $runname.dat
+rm relax.dat supercell.dat
 
 # plot energy vs lattice parameter
 ./vacancy_plot.py $runname.dat
--- lab1/2b/vacancy_plot.py
+++ lab1/2b/vacancy_plot.py
@@ -12,11 +12,11 @@ supercell_sizes, formation_energies = [], []
 for line in dat:
     line = line.strip().split()
     supercell_size = int(line[0].split('_')[-1].split('.')[0])
-    vacancy_energy = float(line[5])
+    vacancy_energy = float(line[4])
     supercell_energy = float(line[-2])
     # calculate formation energy
     formation_energy = vacancy_energy -\
-                       ((supercell_size * 4)**3 - 1) /\
+                       ((supercell_size * 4)**3 -1) /\
                        (supercell_size * 4)**3 *\
                        supercell_energy
     supercell_sizes.append(supercell_size)
@@ -25,7 +25,7 @@ for line in dat:
 # plot supercell size vs formation energy
 fig = plt.figure()
 plt.xlabel('supercell size')
-plt.ylabel('vacancy formation energy (eV)')
+plt.ylabel('relaxed vacancy formation energy (eV)')
 plt.scatter(supercell_sizes, formation_energies, marker='o')
 plt.show()
-plt.savefig('2a.png', format='png')
+plt.savefig('2b.png', format='png')
